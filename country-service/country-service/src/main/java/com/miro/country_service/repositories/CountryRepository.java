cc8b5de0d6a3e47d85dd8a59139c8e4188df2b99
GannouniAmine <153534550+GannouniAmine@users.noreply.github.com>
2025-10-31 09:57:04 +0100

    Fix formatting in CountryRepository interface

--- country-service/country-service/src/main/java/com/miro/country_service/repositories/CountryRepository.java
+++ country-service/country-service/src/main/java/com/miro/country_service/repositories/CountryRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.miro.country_service.beans.Country;
 
-public interface CountryRepository extends JpaRepository<Country,Integer> {
+public interface CountryRepository extends JpaRepository<Country,Integer>  {
 
 }
