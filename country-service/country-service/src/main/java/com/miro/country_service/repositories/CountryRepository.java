8d55a0c92f866fd71a85cae3be72b8f881056450
GannouniAmine <mohamedamine.gannouni@ensi-uma.tn>
2025-11-19 01:07:37 +0100

    update update#	modified:   country-service/country-service/src/main/java/com/miro/country_service/repositories/CountryRepository.java

--- country-service/country-service/src/main/java/com/miro/country_service/repositories/CountryRepository.java
+++ country-service/country-service/src/main/java/com/miro/country_service/repositories/CountryRepository.java
@@ -1,10 +1,10 @@
 package com.miro.country_service.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.miro.country_service.beans.Country;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Integer> {
-    Country findByName(String name);  
+    Country findByName(String name);
 }
