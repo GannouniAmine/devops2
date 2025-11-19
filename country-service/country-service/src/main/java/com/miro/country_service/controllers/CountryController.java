2cae4cf0e4f50eb5e486c42331ab7964f05d2a66
GannouniAmine <mohamedamine.gannouni@ensi-uma.tn>
2025-11-19 01:01:30 +0100

    update

--- country-service/country-service/src/main/java/com/miro/country_service/controllers/CountryController.java
+++ country-service/country-service/src/main/java/com/miro/country_service/controllers/CountryController.java
@@ -16,13 +16,45 @@ public class CountryController {
         this.countryRepository = countryRepository;
     }
 
+   
     @GetMapping
-    public List<Country> getAllCountries() {
+    public List<Country> getCountries() {
         return countryRepository.findAll();
     }
 
+ 
+    @GetMapping("/{id}")
+    public Country getCountryById(@PathVariable int id) {
+        return countryRepository.findById(id).orElse(null);
+    }
+
+ 
+    @GetMapping("/name/{name}")
+    public Country getCountryByName(@PathVariable String name) {
+        return countryRepository.findByName(name);
+    }
+
+
     @PostMapping
-    public Country createCountry(@RequestBody Country country) {
+    public Country addCountry(@RequestBody Country country) {
         return countryRepository.save(country);
+    }
+
+ 
+    @PutMapping("/{id}")
+    public Country updateCountry(@PathVariable int id, @RequestBody Country country) {
+        Country existing = countryRepository.findById(id).orElse(null);
+        if (existing != null) {
+            existing.setName(country.getName());
+            existing.setPopulation(country.getPopulation());
+            return countryRepository.save(existing);
+        }
+        return null;
+    }
+
+ 
+    @DeleteMapping("/{id}")
+    public void deleteCountry(@PathVariable int id) {
+        countryRepository.deleteById(id);
     }
 }
